7c5ff747c0ce692f14e7c41fdfd1049a5fcf4537
voronyata <vorkostya6@gmail.com>
2019-11-28 00:22:13 +0300

    Added logging for the method

--- src/main/java/Test/Softkit/Logik/LogikGetInfoSpecificCompany.java
+++ src/main/java/Test/Softkit/Logik/LogikGetInfoSpecificCompany.java
@@ -4,38 +4,40 @@ import Test.Softkit.Exception.InfoNotFoundException;
 import Test.Softkit.Model.AllInfoSpecificCompany;
 import Test.Softkit.Service.InfoService;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
+
+@Slf4j
 @Component
-public class LogikGetInfoSpecificCompany{
+public class LogikGetInfoSpecificCompany {
 
     @Autowired
     InfoService infoService;
 
-    public List<AllInfoSpecificCompany> infoSpecificCompanies(String company)  throws IOException {
-    RestTemplate restTemplate = new RestTemplate();
-    ResponseEntity<String> response
-            = restTemplate.getForEntity("https://sandbox.iexapis.com/stable/stock/" + company.toUpperCase() + "/quote?token=Tpk_ee567917a6b640bb8602834c9d30e571", String.class);
+    public List<AllInfoSpecificCompany> infoSpecificCompanies(String company) throws IOException {
+        RestTemplate restTemplate = new RestTemplate();
+        log.info("Name of the company ", company);
+        ResponseEntity<String> response
+                = restTemplate.getForEntity("https://sandbox.iexapis.com/stable/stock/" + company.toUpperCase() + "/quote?token=Tpk_ee567917a6b640bb8602834c9d30e571", String.class);
         if (response.getStatusCode() != HttpStatus.OK) {
-        throw new InfoNotFoundException("STOP FULL");
-    }
+            throw new InfoNotFoundException("STOP FULL");
+        }
 
-    List<AllInfoSpecificCompany> companyList = Arrays.asList(new ObjectMapper().readValue(response.getBody(),
-            AllInfoSpecificCompany.class));
+        List<AllInfoSpecificCompany> companyList = Arrays.asList(new ObjectMapper().readValue(response.getBody(),
+                AllInfoSpecificCompany.class));
         for (AllInfoSpecificCompany allInfoSpecificCompany : companyList) {
-        System.out.println(allInfoSpecificCompany);
-        infoService.save(allInfoSpecificCompany);
-    }
+            System.out.println(allInfoSpecificCompany);
+            infoService.save(allInfoSpecificCompany);
+        }
         return companyList;
-}
+    }
 
 }
